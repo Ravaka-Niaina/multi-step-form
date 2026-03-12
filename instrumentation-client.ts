3d52b79b8aca61fba966a1dceed16bf06b119c71
Ravaka Niaina <ratefyniaina@gmail.com>
2026-03-12 14:02:14 +0300

    add sentry to monitor syntax errors and stuff

---
+++ instrumentation-client.ts
@@ -0,0 +1,20 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://bd141a5a04a822be9feb4bf205148fa8@o4511027104382976.ingest.de.sentry.io/4511027106545744",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
